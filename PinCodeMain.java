06aa16a8ddbc39e6a76d22d340bf6dcdd2b4cd11
shubham <shubhamkumar10516@gmail.com>
2020-09-29 00:03:16 +0530

    [Shubham]ADDED: pin code checking with only 6 digits

---
+++ PinCodeMain.java
@@ -0,0 +1,27 @@
+package com.academy.kant.PinCode;
+import java.util.*;
+
+public class PinCodeMain {
+
+	public static boolean validatePinCode(String pin) {
+		
+		String pattern = "[1-9]{1}[0-9].{5}";
+		return pin.matches(pattern);
+		
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+		System.out.println("Enter pincode");
+        String pin = sc.nextLine();
+        
+        while(!validatePinCode(pin)) {
+			System.out.println("Invalid!! Enter first name again: ");
+			pin = sc.nextLine() ;
+			
+		}
+        
+		
+	}
+}
